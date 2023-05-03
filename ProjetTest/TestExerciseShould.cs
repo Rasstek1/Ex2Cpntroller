93be6c8814b70b70b4bc6f38461cd25bab340b1c
Martin Racine <2295093@cegepoutaouais.qc.ca>
2023-05-03 19:29:21 -0400

    Fin des test unitaire

--- ProjetTest/TestExerciseShould.cs
+++ ProjetTest/TestExerciseShould.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnPetitProjet.Controllers;
+using UnPetitProjet.Models;
 using Xunit;
 
 namespace ControllerTest
@@ -13,7 +14,7 @@ namespace ControllerTest
     public class TestExerciseShould
     {
         [Fact]
-        public void TestBienvenue()
+        public void TestBienvenue()//succeed
         {
             // Arrange
             var exerciseController = new ExerciceController();
@@ -26,7 +27,7 @@ namespace ControllerTest
             Assert.Equal("<h2>Bienvenue dans mon site web<h2>", contentResult.Content);
         }
         [Fact]
-        public void TestBienvenue2()
+        public void TestBienvenue2()//succeed
         {
             // Declare les variables pour le test unitaire
             var exerciseController = new ExerciceController();
@@ -50,7 +51,7 @@ namespace ControllerTest
 
         [Fact]
 
-        public void TestIndex2()
+        public void TestIndex2()//succeed
         {
             // Arrange : préparer le test en instanciant le contrôleur
             var exerciseController = new ExerciceController();
@@ -58,11 +59,59 @@ namespace ControllerTest
             // Act : appeler la méthode d'action Index2 du contrôleur
             var result = exerciseController.Index2() as ViewResult;
 
-            // Assert : vérifier que la vue renvoyée a le bon nom
+            // Assert : vérifier que la vue renvoyée a le bon nom et le bon type
             Assert.NotNull(result);
-            Assert.Equal("MaVue", result.ViewName);
+            Assert.IsType<ViewResult>(result);
+            var viewResult = result as ViewResult;
+            Assert.Equal("MaVue", viewResult.ViewName);
+        }
+
+        [Fact]
+
+        public void ListeClients()//fail
+        {
+            // Arrange : préparer le test en instanciant le contrôleur
+            var exerciseController = new ExerciceController();
+
+            // Act : appeler la méthode d'action ListeClients du contrôleur
+            var result = exerciseController.ListeClients() as JsonResult;
+
+            // Assert : vérifier que le résultat renvoyé est bien un objet JsonResult avec un ContentType approprié et des données valides
+            Assert.NotNull(result);
+            Assert.IsType<JsonResult>(result);
+            var jsonResult = result as JsonResult;
+            Assert.NotNull(jsonResult.ContentType);
+            Assert.Equal("application/json", jsonResult.ContentType);
+            var clients = jsonResult.Value as List<Client>;
+            Assert.NotNull(clients);
+            Assert.Equal(1, clients.Count);
+            Assert.Equal(1, clients[0].Numero);
+            Assert.Equal("Camara", clients[0].Nom);
+            Assert.Equal("Oumar", clients[0].Prenom);
+            Assert.Equal("oumar@gmail.com", clients[0].Courriel);
+        }
+
+        [Fact]
+        public void TestRedirigeListeClient()//succeed
+        {
+            // Arrange : préparer le test en instanciant le contrôleur
+            var exerciseController = new ExerciceController();
+
+            // Act : appeler la méthode d'action RedirigeListeClient du contrôleur
+            var result = exerciseController.RedirigeListeClient() as RedirectToActionResult;
+
+            // Assert : vérifier que le résultat renvoyé est bien une redirection vers l'action ListeClients
+            Assert.NotNull(result);
+            Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("ListeClients", result.ActionName);
+
+
         }
 
     }
-
 }
+
+
+
+
+
