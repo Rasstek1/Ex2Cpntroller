bec5381ad232824bc5135c39768cad431dd90db0
Martin Racine <2295093@cegepoutaouais.qc.ca>
2023-05-04 14:46:04 -0400

    yo

--- ProjetTest/TestExerciseShould.cs
+++ ProjetTest/TestExerciseShould.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Routing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +15,7 @@ namespace ControllerTest
     public class TestExerciseShould
     {
         [Fact]
-        public void TestBienvenue()//succeed
+        public void TestBienvenue()//Test 1 succeed
         {
             // Arrange
             var exerciseController = new ExerciceController();
@@ -27,7 +28,7 @@ namespace ControllerTest
             Assert.Equal("<h2>Bienvenue dans mon site web<h2>", contentResult.Content);
         }
         [Fact]
-        public void TestBienvenue2()//succeed
+        public void TestBienvenue2()//Test 2 succeed
         {
             // Declare les variables pour le test unitaire
             var exerciseController = new ExerciceController();
@@ -51,7 +52,7 @@ namespace ControllerTest
 
         [Fact]
 
-        public void TestIndex2()//succeed
+        public void TestIndex2()//Test 3 succeed 
         {
             // Arrange : préparer le test en instanciant le contrôleur
             var exerciseController = new ExerciceController();
@@ -67,14 +68,46 @@ namespace ControllerTest
         }
 
         [Fact]
+        public void GetFichierPdf_ReturnsFileResult()
+        {
+            // Arrange
+            var exerciceController = new ExerciceController();
+            var expectedFileName = "chemnement.pdf";
+            var expectedContentType = "application/pdf";
+            var expectedFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "pdf", expectedFileName);
 
-        public void ListeClients()//fail
+            // Act
+            var result = exerciceController.GetFichierPdf() as FileContentResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(expectedContentType, result.ContentType);
+
+            var actualFilePath = Path.GetFullPath(expectedFilePath);
+            Assert.Equal(actualFilePath, result.FileDownloadName);
+
+            var actualFileContent = System.IO.File.ReadAllBytes(actualFilePath);
+            Assert.Equal(actualFileContent.Length, result.FileContents.Length);
+
+            for (int i = 0; i < actualFileContent.Length; i++)
+            {
+                Assert.Equal(actualFileContent[i], result.FileContents[i]);
+            }
+        }
+
+
+
+
+
+        [Fact]
+
+        public void ListeClients()//Test 4 fail
         {
             // Arrange : préparer le test en instanciant le contrôleur
             var exerciseController = new ExerciceController();
 
             // Act : appeler la méthode d'action ListeClients du contrôleur
-            var result = exerciseController.ListeClients() as JsonResult;
+            var result = exerciseController.ListeClients() as JsonResult;//'as' test que le type de retour est bien un JsonResult
 
             // Assert : vérifier que le résultat renvoyé est bien un objet JsonResult avec un ContentType approprié et des données valides
             Assert.NotNull(result);
@@ -92,7 +125,7 @@ namespace ControllerTest
         }
 
         [Fact]
-        public void TestRedirigeListeClient()//succeed
+        public void TestRedirigeListeClient()//Test 5 succeed
         {
             // Arrange : préparer le test en instanciant le contrôleur
             var exerciseController = new ExerciceController();
@@ -106,12 +139,158 @@ namespace ControllerTest
             Assert.Equal("ListeClients", result.ActionName);
 
 
+
         }
 
     }
 }
 
+//EXEMPLE DE TESTS UNITAIRES  
+/*using ControleursExercices.Controllers;
+using ControleursExercices.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xunit;
+
+namespace ControleursExercicesTests.Controllers
+{
+    public class ControleursExercicesShould
+    {
+
+        [Fact]
+        public void IndexShouldReturnViewResult()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.Index();
+
+            Assert.IsType<ViewResult>(result);
+
+        }
+
+        [Fact]
+        public void ShowTextString()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.ShowText();
+
+            var expectedResult = "<h2>Bienvenu dans mon site web</h2>";
+            Assert.IsType<string>(result);
+            Assert.Equal(expectedResult, result);
+        }
 
 
+        [Fact]
+        public void ReturnContent()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.SendContent();
+
+            Assert.IsType<ContentResult>(result);
+
+            var expectedContent = "<h2>Bienvenu dans mon site web</h2>";
+            var content = Assert.IsType<string>(result.Content);
+            Assert.Equal(expectedContent, content);
+
+            var expectedContentType = "text/html";
+            var contentType = Assert.IsType<string>(result.ContentType);
+            Assert.Equal(expectedContentType, contentType);
+        }
 
 
+        [Fact]
+        public void ReturnTextWithParamValues()
+        {
+            var homeController = new HomeController();
+
+            string id = "1234";
+            string name = "Dalicia";
+            var result = homeController.TextWithParamValues(id, name);
+
+            var expectedResult = "La valeur de id est : " + id + " et le nom est name = " + name;
+
+            Assert.IsType<string>(result);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void ShowIndexView()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.Index();
+
+            var viewResult = Assert.IsAssignableFrom<ActionResult>(result);
+        }
+
+
+        [Fact]
+        public void AfficherPdfShouldReturnVirtualFileResult()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.AfficherPdf();
+
+            Assert.IsType<VirtualFileResult>(result);
+
+            var expectedFileName = @"~\Files\dummy.pdf";
+            Assert.Equal(expectedFileName, result.FileName);
+
+            var expectedContentType = "application/pdf";
+            Assert.Equal(expectedContentType, result.ContentType);
+        }
+
+        [Fact]
+        public void AfficherJsonShouldReturnJsonResult()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.AfficherJson();
+
+            Assert.IsType<JsonResult>(result);
+
+            dynamic valueJson = result.Value;
+
+            Client expectedClient1 = new Client(1, "N1", "P1", "np1@gmail.com");
+            Client expectedClient2 = new Client(2, "N2", "P2", "np2@gmail.com");
+
+            var jsonElement1 = valueJson[0];
+            var jsonElement2 = valueJson[1];
+
+            Assert.IsType<Client>(jsonElement1);
+            Assert.IsType<Client>(jsonElement2);
+
+            Assert.Equal<int>(jsonElement1.Numero, expectedClient1.Numero);
+            Assert.Equal(jsonElement1.Nom, expectedClient1.Nom);
+            Assert.Equal(jsonElement1.Prenom, expectedClient1.Prenom);
+            Assert.Equal(jsonElement1.Courriel, expectedClient1.Courriel);
+
+            Assert.Equal<int>(expectedClient2.Numero, jsonElement2.Numero);
+            Assert.Equal(expectedClient2.Nom, jsonElement2.Nom);
+            Assert.Equal(expectedClient2.Prenom, jsonElement2.Prenom);
+            Assert.Equal(expectedClient2.Courriel, jsonElement2.Courriel);
+        }
+
+        [Fact]
+        public void RedirigerShouldReturnStringActionResult()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.RedirectToShowText();
+            var viewResult = Assert.IsType<RedirectToActionResult>(result);
+
+            var expectedActionName = "ShowText";
+            var actionName = Assert.IsType<string>(viewResult.ActionName);
+            Assert.Equal(expectedActionName, actionName);
+        }
+    }
+}
+
+*/
